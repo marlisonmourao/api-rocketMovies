f546f6aaddd3fc232ebf7c2f6edec86a4959ed90
marlisonmourao <bentesmourao@gmail.com>
2023-03-11 16:44:47 -0400

    treating exceptions

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
+require("express-async-errors")
 const express = require('express')
 const database = require('./database/sqlite')
+
+const AppError = require('./utils/AppError')
 
 const routes = require('./routes')
 
@@ -7,6 +10,23 @@ const app = express()
 app.use(express.json())
 
 app.use(routes)
+
+
+app.use((error, request, response, next) => {
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: "error",
+      message: error.message,
+    });
+  }
+
+  console.error(error);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
 
 database()
 
