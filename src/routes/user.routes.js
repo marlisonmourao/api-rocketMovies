f87e9af9d84d6545e4552a575f2d9584e24202d2
marlisonmourao <bentesmourao@gmail.com>
2023-03-11 17:02:39 -0400

    updating user data

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -7,5 +7,6 @@ const usersRoutes = Router()
 const userController = new UserController()
 
 usersRoutes.post("/", userController.create)
+usersRoutes.put("/:id", userController.update)
  
 module.exports = usersRoutes
