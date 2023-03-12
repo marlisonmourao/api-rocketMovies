0b4bf5ac803c7af5f9b579f5e7f3b563cfb12448
marlisonmourao <bentesmourao@gmail.com>
2023-03-11 23:16:33 -0400

    updating user password

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,4 @@
-const { hash } = require('bcryptjs');
+const { hash, compare } = require('bcryptjs');
 const AppError = require('../utils/AppError')
 
 const sqliteConnection = require("../database/sqlite")
@@ -25,11 +25,12 @@ class UserController {
   }
 
   async update(request, response) {
-    const { name, email } = request.body
+    const { name, email, password, old_password } = request.body
     const { id } = request.params
 
     const database = await sqliteConnection()
-    const user = database.get("SELECT * FROM users WHERE id = (?)", [id])
+    
+    const user = await database.get("SELECT * FROM users WHERE id = (?)", [id])
 
     if(!user) {
       throw new AppError("Usúario não encontrado.")
@@ -37,8 +38,22 @@ class UserController {
 
     const userWithUpdateEmail = await database.get("SELECT * FROM users WHERE email = (?)", [email])
 
-    if(!userWithUpdateEmail && userWithUpdateEmail.id !== user.id ) {
+    if(userWithUpdateEmail && userWithUpdateEmail.id !== user.id) {
       throw new AppError("Este e-mail já em uso.")
+    }
+
+    if(password && !old_password) {
+      throw new AppError("Você precisa informar a senha antiga para definir a nova senha.")
+    }
+
+    if(password && old_password) {
+      const checkOldPassword = await compare(old_password, user.password)
+
+      if(!checkOldPassword) {
+        throw new AppError("A senha antiga não confere.")
+      }
+
+      user.password = await hash(password, 8)
     }
 
     user.name = name;
@@ -48,10 +63,10 @@ class UserController {
       UPDATE users SET 
       name = ?,
       email = ?,
-      updated_at = ? 
-      WHERE id = ?
-    `, 
-    [user.name, user.email, new Date(), id])
+      password = ?,
+      updated_at = ?
+      WHERE id = ?`,
+      [user.name, user.email, user.password, new Date(), id])
 
     return response.json()
   }
