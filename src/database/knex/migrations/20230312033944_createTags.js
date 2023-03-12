7f43552ea7b2f8c26c12179fa40d1d23f033f0c6
marlisonmourao <bentesmourao@gmail.com>
2023-03-11 23:49:04 -0400

    create table tags

---
+++ src/database/knex/migrations/20230312033944_createTags.js
@@ -0,0 +1,12 @@
+exports.up = knex => knex.schema.createTable("tags", (table) => {
+
+  table.increments("id")
+  table.integer("note_id").references("id").inTable("notes")
+  table.integer("user_id").references("id").inTable("users")
+  table.text("name")
+})
+
+
+exports.down = knex => knex.schema.dropTable("tags", (table) => {
+
+}) 
