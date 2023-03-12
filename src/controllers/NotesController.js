d477a931da232581fa092d18e4528f05b0feb67b
marlisonmourao <bentesmourao@gmail.com>
2023-03-12 19:32:57 -0400

    deleting note from database

--- src/controllers/NotesController.js
+++ src/controllers/NotesController.js
@@ -38,6 +38,14 @@ class NotesController {
       tags
     })
   }
+
+  async delete(request, response) {
+    const { id } = request.params
+
+    await knex("notes").where({ id }).delete()
+
+    return response.json()
+  }
 }
 
 module.exports = NotesController
