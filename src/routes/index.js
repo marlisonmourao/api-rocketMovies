a3321ff46de206660981925843ec6380dddeca3f
marlisonmourao <bentesmourao@gmail.com>
2023-03-13 01:20:17 -0400

    listing tags

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ const { Router } = require('express')
 
 const usersRouter = require('./user.routes')
 const notesRouter = require("./notes.routes");
+const tagsRouter = require("./tags.routes");
 
 const router = Router()
 
 router.use('/users', usersRouter)
 router.use("/notes", notesRouter);
+router.use("/tags/", tagsRouter);
 
 module.exports = router
